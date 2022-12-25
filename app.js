a2a8f47784b733309bcac8f601e914de881ebad5
Ezequias <ezequias.8ps@gmail.com>
2022-12-25 02:33:20 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 // mais um console log de teste:
 console.log("Este comando foi incluido pela web para ser feito o pull localmente")
+console.log("Enviando para o GitHub")
+
